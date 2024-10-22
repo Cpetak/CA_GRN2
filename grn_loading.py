5a2aa170f0a2c53e06491e883141a7fbfecd7d49
Csenge Petak <csenge.petak@uvm.edu>
2024-10-22 09:43:27 -0400

    updating grn loading

--- grn_loading.py
+++ grn_loading.py
@@ -5,34 +5,65 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import helper
+import os
 
 # %%
 # Load GRN
 
 # Parameters
-rule = 22
-id = 1
+rule = 102
 grn_size = 22
 num_cells = 22
 dev_steps = 22
 geneid = 1 #which gene was used to get fitness
+num_reps = 10
 
-filename = f"results_new_rules/stats_300_{rule}-{rule}_69904-149796_{id}" + "_best_grn.txt"
-grns = np.loadtxt(filename)
-num_grns = int(grns.shape[0]/(grn_size+2)/grn_size)
-grns = grns.reshape(num_grns,grn_size+2,grn_size)
+root="/Users/csengepetak/best_grn_results/"
 
-if len(grns.shape) == 2:
-    grns = np.expand_dims(grns, axis=0)
-print(grns.shape)
+vari_grns=[np.loadtxt(os.path.expanduser(root+f"stats_300_{rule}-{rule}_{69904}-{149796}_{i+1}_best_grn.txt")) for i in range(num_reps)]
+temp_grns = []
+for grn in vari_grns:
+    num_grns = int(grn.shape[0]/(grn_size+2)/grn_size)
+    new_grn = grn.reshape(num_grns,grn_size+2,grn_size)
+    new_grn = new_grn[:9899,:,:] #standardize num generations
+    temp_grns.append(new_grn)
+vari_grns=np.array(temp_grns)
+print(vari_grns.shape) # repeats, generations, gene matrix dim1 and dim2
+
+#%%
+
+static1_grns=[np.loadtxt(os.path.expanduser(root+f"stats_100000_{rule}_69904_{i+1}_best_grn.txt")) for i in range(num_reps)]
+temp_grns = []
+for grn in static1_grns:
+    num_grns = int(grn.shape[0]/(grn_size+2)/grn_size)
+    new_grn = grn.reshape(num_grns,grn_size+2,grn_size)
+    new_grn = new_grn[:9899,:,:] #standardize num generations
+    temp_grns.append(new_grn)
+static1_grns=np.array(temp_grns)
+print(static1_grns.shape)
+
+static2_grns=[np.loadtxt(os.path.expanduser(root+f"stats_100000_{rule}_149796_{i+1}_best_grn.txt")) for i in range(num_reps)]
+temp_grns = []
+for grn in static2_grns:
+    num_grns = int(grn.shape[0]/(grn_size+2)/grn_size)
+    new_grn = grn.reshape(num_grns,grn_size+2,grn_size)
+    new_grn = new_grn[:9899,:,:] #standardize num generations
+    temp_grns.append(new_grn)
+static2_grns=np.array(temp_grns)
+print(static2_grns.shape)
+
+
+#if len(grns.shape) == 2:
+    #grns = np.expand_dims(grns, axis=0)
+#print(grns.shape)
 
 
 
 # %%
 # Get target, phenotype, and fitnesses
-seed_int=1024 
+seed_int=69904 
 #binary of initial condition of fitness gene. 1024 is 1 seed in the middle. other frequently used initial conditions: 69904, 149796
-targets, phenos, fitnesses = helper.get_pop_TPF(grns, len(grns), num_cells, grn_size, dev_steps, geneid, rule, seed_int)
+targets, phenos, fitnesses = helper.get_pop_TPF(vari_grns, len(vari_grns), num_cells, grn_size, dev_steps, geneid, rule, seed_int_target = seed_int, seed_int_dev = seed_int)
 
 print(targets.shape)
 print(phenos.shape)
